b5cbf82b1f60561f5a646977b44452a88f0e48f8
ezuykow <zuy1996@gmail.com>
2022-11-15 18:53:10 +0300

    Initial commit

---
+++ src/main/java/ru/ezuykow/employee_book/EmployeeBookSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.ezuykow.employee_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeBookSpringApplication.class, args);
+    }
+
+}
